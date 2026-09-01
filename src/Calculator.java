9a52c9fbd4a013855df3a758d451f88268a20413
Lekshman Sujesh  R <lexcyclops@gmail.com>
2026-09-01 19:14:45 +0530

    Created a display screen using JLabel

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,28 @@
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class Calculator {
     public Calculator() {
         JFrame jf=new JFrame("Calculator");
         jf.setLayout(null);
         jf.setSize(600,600);
+        jf.setLocation(300,150);
+
+        JLabel displayLabel = new JLabel("Hello");
+        displayLabel.setBounds(25,50,540,40);
+        displayLabel.setBackground(Color.GRAY);
+        displayLabel.setOpaque(true);
+        displayLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        displayLabel.setForeground(Color.white);
+        
+        jf.add(displayLabel);
+
+
+
+
         jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
